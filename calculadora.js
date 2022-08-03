32e23902d4c3c35d0a3be53ee6211c267d8c46b7
João Batista <110290138+jbcosme@users.noreply.github.com>
2022-08-03 18:29:46 -0300

    ver 1.2.4
    Ajustado calculo do imc para 2 decimais

--- calculadora.js
+++ calculadora.js
@@ -1,4 +1,4 @@
 function calcBMI() {
     var weight = document.bmiform.pounds.value, height = document.bmiform.inches.value;
-    document.bmiform.bmi.value = parseInt((weight) / (height * height / 10000));
+    document.bmiform.bmi.value = parseFloat((weight) / (height * height / 10000)).toFixed(2);
   } 
