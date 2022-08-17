f3693a7f6606226683e5412f73e4085db9896344
João Batista <110290138+jbcosme@users.noreply.github.com>
2022-08-17 20:33:44 -0300

    concluido pagina 2

--- expectativa.js
+++ expectativa.js
@@ -1,5 +1,9 @@
 function calcBMI() {
-    var pinicial = document.bmiform.pinicial.value, altura = document.bmiform.inches.value, p_atual = document.bmiform.p_atual.value, altura2 = (altura * altura / 10000);
+    var pinicial = document.bmiform.pinicial.value, altura = document.bmiform.inches.value, p_atual = document.bmiform.p_atual.value, altura2 = (altura * altura / 10000), p_perdido = (pinicial - p_atual);
     document.bmiform.imc_inicial.value = parseFloat((pinicial) / (altura2)).toFixed(2);
     document.bmiform.imc_atual.value = parseFloat((p_atual) / (altura2)).toFixed(2);
+    document.bmiform.p_ideal.value = parseFloat(altura2 * 25).toFixed(2);
+    document.bmiform.p_excesso.value = ((pinicial) - (document.bmiform.p_ideal.value));
+    document.bmiform.p_esperado.value = parseFloat((pinicial) - (document.bmiform.p_excesso.value * 0.7)).toFixed(2);
+    document.bmiform.ppep.value = ((p_perdido /  document.bmiform.p_excesso.value) *100).toFixed(2);
   } 
